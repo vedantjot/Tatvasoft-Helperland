138780f6110a661a43efe31f075984b1945cf407
vedantjot <vedantjot@gmail.com>
2021-11-29 15:10:03 +0530

    FAQ and JS added

---
+++ HTML/js/faq.js
@@ -0,0 +1,14 @@
+function changeImage() {
+
+   
+
+    if (document.getElementById("faq-bullet-img").src =="image/down-arrow.png") 
+    {
+        document.getElementById("faq-bullet-img").src = "image/rigth-arrow.png";
+        
+    }
+    else 
+    {
+        document.getElementById("faq-bullet-img").src = "image/down-arrow.png";
+    }
+}
