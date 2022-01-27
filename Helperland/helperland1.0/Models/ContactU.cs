5f679393f5fdcd189c21573f7d24344f1105a415
vedantjot <vedantjot@gmail.com>
2022-01-27 13:18:52 +0530

    SQl and .net added

---
+++ Helperland/helperland1.0/Models/ContactU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace helperland1._0.Models
+{
+    public partial class ContactU
+    {
+        public int ContactUsId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string SubjectType { get; set; }
+        public string Subject { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+        public string UploadFileName { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? Status { get; set; }
+        public int? Priority { get; set; }
+        public int? AssignedToUser { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Helperland/helperland1.0/Models/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace helperland1._0.Models
+{
+    public partial class State
+    {
+        public State()
+        {
+            Cities = new HashSet<City>();
+        }
+
+        public int Id { get; set; }
+        public string StateName { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
